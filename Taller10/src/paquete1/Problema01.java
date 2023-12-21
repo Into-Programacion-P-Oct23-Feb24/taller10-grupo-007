1936e2da3c8e9af48e7fe949f1dabd6f6ccbb3ae
CisneParra <adparra2@utpl.edu.ec>
2023-12-21 15:34:02 -0500

    Cambios del taller

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -15,7 +15,36 @@ public class Problema01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double[][] notas = {{9, 10, 7, 8},
+        {10, 5, 7, 9},
+        {7, 9.4, 10, 9}};
+        String[] nombresEstudiante = {"Jerry Ponce", "Gabriela Lewis", "David Bell"};
+        double[] promedios = new double[3];
+        double promedio = 0;
+
+        String reporte = "";
+
+        for (int i = 0; i < notas.length; i++) {
+            double suma = 0;
+            for (int j = 0; j < notas[i].length; j++) {
+                suma = suma + notas[i][j];
+                
+
+            }
+            promedio = suma / 4;
+            promedios[i] = promedio; // 
+        }
+
+        // Presentación de resultados
+        for (int i = 0; i < nombresEstudiante.length; i++) {
+            reporte = String.format("%sEstudiante: %s tiene un promedio de "
+                    + "%.2f\n",
+                    reporte,
+                    nombresEstudiante[i],
+                    promedios[i]);
+        }
+
+        System.out.printf("%s\n", reporte);
     }
-    
+
 }
